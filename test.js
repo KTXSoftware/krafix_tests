35ad7bd4058ee9cbd18dae04241b632f8f9d4abf
Robert Konrad <robert@ktx-software.com>
2015-08-08 22:47:25 +0200

    Updated test script for non windows systems.

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 let child_process = require('child_process');
 let fs = require('fs');
+let os = require('os');
 let path = require('path');
 
 let files = fs.readdirSync('in');
@@ -12,7 +13,10 @@ if (!fs.existsSync('temp')) fs.mkdirSync('temp');
 for (let file of files) {
 	if (file.endsWith('.glsl')) {
 		console.log('Compiling ' + file);
-		let targets = ['glsl', 'd3d9'];
+		let targets = ['glsl'];
+		if (os.platform() === 'win32') {
+			targets.push('d3d9');
+		}
 		for (let target of targets) {
 			if (!fs.existsSync(path.join('out', target))) fs.mkdirSync(path.join('out', target));
 			let outfile = path.join('out', target, file.substr(0, file.lastIndexOf('.')));
@@ -20,8 +24,8 @@ for (let file of files) {
 				outfile = path.join('out', target, file.substr(0, file.lastIndexOf('.')) + '.' + target);
 			}
 			try {
-				child_process.execFileSync(path.normalize('../build/Debug/krafix.exe'), [target, path.join('in', file), outfile, 'temp', 'windows']);
-				if (target === 'glsl') {
+				child_process.execFileSync(path.normalize('../build/Debug/krafix'), [target, path.join('in', file), outfile, 'temp', 'windows']);
+				if (target === 'glsl' && os.platform() === 'win32') {
 					child_process.execFileSync(path.normalize('../glslang/Install/Windows/glslangValidator.exe'), [outfile]);
 				}
 			}
